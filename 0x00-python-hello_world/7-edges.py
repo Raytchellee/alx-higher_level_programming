94e32ae5bdd42763f8a9572fbae86a1ab56b9d0f
raytchellee <amakaoforkaja@gmail.com>
2023-07-10 06:06:41 -0700

    7-edges.py

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+word_first_3 = f"{word[:3]:s}"
+word_last_2 = f"{word[-2:]:s}"
+middle_word = f"{word[1: -1]:s}"
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 letters: {word_last_2}")
+print(f"Middle word: {middle_word}")
