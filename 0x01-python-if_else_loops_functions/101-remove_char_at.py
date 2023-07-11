79bdd7f7868ae8891482c3e4eeacac1759dcdc83
raytchellee <amakaoforkaja@gmail.com>
2023-07-11 16:12:55 -0700

    remove char

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,9 @@
+# #!/usr/bin/python3
+def remove_char_at(str, n):
+    new_str = ""
+    for idx in range(len(str)):
+        if (idx == n):
+            continue
+        else:
+            new_str += str[idx]
+    return new_str
