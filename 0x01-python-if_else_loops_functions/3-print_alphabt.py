71633368e07dbeb552f622fc34e0b7264ec367a9
raytchellee <amakaoforkaja@gmail.com>
2023-07-11 13:04:08 -0700

    alph minus q and e

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for idx in range(97, 123):
-    if(chr(idx) != 'e' and chr(idx) != 'q'):
+    if (chr(idx) != 'e' and chr(idx) != 'q'):
         print("{}".format(chr(idx)), end="")
