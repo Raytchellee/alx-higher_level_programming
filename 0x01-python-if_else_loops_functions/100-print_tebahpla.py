87dfc660fda661f7eb621d000780b4bce5b6e64e
raytchellee <amakaoforkaja@gmail.com>
2023-07-11 15:30:29 -0700

    rev alphabets

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,9 @@
+def print_rev_alph():
+    for idx in range(ord('Z'), ord('A') - 1, -1):
+        if (idx % 2 == 0):
+            char = chr(idx + 32)
+        else:
+            char = chr(idx)
+        print("{}".format(char), end='')
+
+print_rev_alph()
