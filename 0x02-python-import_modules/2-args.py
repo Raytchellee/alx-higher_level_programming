2aba643711005ff10bbb6ef02fe2681243a42335
raytchellee <amakaoforkaja@gmail.com>
2023-07-13 04:19:00 -0700

    task 3

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,8 +3,9 @@ if __name__ == "__main__":
     from sys import argv
 
     length = len(argv)
-
-    if (length == 2):
+    if (length == 1):
+        print("{} arguments.".format(length - 1))
+    elif (length == 2):
         print("{} argument:".format(length - 1))
     else:
         print("{} arguments:".format(length - 1))
