bfbba8013ff359bc2a61351e2cd71eaf6ecf3b1b
raytchellee <amakaoforkaja@gmail.com>
2023-07-16 05:30:07 -0700

    check

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,4 +3,4 @@ def multiple_returns(sentence):
     if len(sentence) == 0:
         return tuple((0, None))
     else:
-        return tuple((len(sentence),sentence[0]))
+        return tuple((len(sentence), sentence[0]))
