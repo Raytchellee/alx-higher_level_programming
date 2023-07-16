eb9f70178b6e5f1c384467c78b606632f0286f1d
raytchellee <amakaoforkaja@gmail.com>
2023-07-16 07:24:00 -0700

    test without python header

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,28 @@
+#include <Python.h>
+
+/**
+ * print_python_list_info - prints some basic info about Python lists
+ * @p: input value
+ * Return: none
+ */
+
+void print_python_list_info(PyObject *p)
+{
+	PyObject *entry;
+	int idx, count, space;
+
+	space = ((PyListObject *)p)->allocated;
+	count = Py_SIZE(p);
+
+	printf("[*] Size of the Python List = %i\n", count);
+	printf("[*] Allocated = %i\n", space);
+
+	while (idx < count)
+	{
+		printf("Element %i: ", idx);
+		entry = PyList_GetItem(p, idx);
+		printf("%s\n", Py_TYPE(entry)->tp_name);
+
+        idx++
+	}
+}
