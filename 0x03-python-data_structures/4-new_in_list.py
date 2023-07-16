879d11ee378fd0000cc3479d1b302181b608f240
raytchellee <amakaoforkaja@gmail.com>
2023-07-16 04:44:16 -0700

    changes

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     if (idx < 0):
-        return None
+        return my_list
     if (idx >= len(my_list)):
-        return None
+        return my_list
     dupe = my_list[:]
     dupe[idx] = element
 
