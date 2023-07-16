cbca16e4c130b99fecdf0c144b61f1bd3ca81732
raytchellee <amakaoforkaja@gmail.com>
2023-07-16 04:50:57 -0700

    6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix is None:
-        print("", end="")
-    else:
+    if matrix:
         for idx in range(len(matrix)):
             for idy in range(len(matrix[idx])):
                 if (idy != len(matrix[idx]) - 1):
