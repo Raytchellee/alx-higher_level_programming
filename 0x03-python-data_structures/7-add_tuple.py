a0ef2394ce73ccc6fcdee4c4c17a862361150040
raytchellee <amakaoforkaja@gmail.com>
2023-07-16 05:25:22 -0700

    check

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    list_1 = list(tuple_a)
+    if len(list_1) < 2:
+        for idx in range(len(list_1), 2):
+            list_1.append(0)
+
+    list_2 = list(tuple_b)
+    if len(list_2) < 2:
+        for idx in range(len(list_2), 2):
+            list_2.append(0)
+
+    e = list_1[0] + list_2[0]
+    f = list_1[1] + list_2[1]
+
+    return tuple((e, f))
