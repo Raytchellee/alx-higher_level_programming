21bf330bb7a1fdab50288f03c96c2a0c1abec595
raytchellee <amakaoforkaja@gmail.com>
2023-07-19 20:27:13 +0000

    task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,20 +4,19 @@ def roman_to_int(roman_string):
         return 0
     total = 0
     t = 0
-    l = len(roman_string)
-    while t < l:
+    lt = len(roman_string)
+    while t < lt:
         idx = roman_string[t]
         if (idx == 'I'):
-            if (t + 1 < l 
-                and roman_string[t + 1] == 'V'):
+            if (t + 1 < lt and roman_string[t + 1] == 'V'):
                 t += 1
                 total += 4
-            elif (t + 1 < l and roman_string[t + 1] == 'X'):
+            elif (t + 1 < lt and roman_string[t + 1] == 'X'):
                 t += 1
                 total += 9
-            elif (t + 1 < l and roman_string[t + 1] == 'I'):
+            elif (t + 1 < lt and roman_string[t + 1] == 'I'):
                 count = 1
-                while (t < l and t + 1 < l and roman_string[t + 1] == 'I'):
+                while (t < lt and t + 1 < lt and roman_string[t + 1] == 'I'):
                     count = count + 1
                     t += 1
                 total += count
@@ -27,15 +26,15 @@ def roman_to_int(roman_string):
         elif (idx == 'V'):
             total += 5
         elif (idx == 'X'):
-            if (t + 1 < l and roman_string[t + 1] == 'L'):
+            if (t + 1 < lt and roman_string[t + 1] == 'L'):
                 t += 1
                 total += 40
-            elif (t + 1 < l and roman_string[t + 1] == 'C'):
+            elif (t + 1 < lt and roman_string[t + 1] == 'C'):
                 t += 1
                 total += 90
-            elif (t + 1 < l and roman_string[t + 1] == 'X'):
+            elif (t + 1 < lt and roman_string[t + 1] == 'X'):
                 count = 10
-                while (t < l and t + 1 < l and roman_string[t + 1] == 'X'):
+                while (t < lt and t + 1 < lt and roman_string[t + 1] == 'X'):
                     count = count + 10
                     t += 1
                 total += count
@@ -44,15 +43,15 @@ def roman_to_int(roman_string):
         elif (idx == 'L'):
             total += 50
         elif (idx == 'C'):
-            if (t + 1 < l and roman_string[t + 1] == 'D'):
+            if (t + 1 < lt and roman_string[t + 1] == 'D'):
                 t += 1
                 total += 400
-            elif (t + 1 < l and roman_string[t + 1] == 'M'):
+            elif (t + 1 < lt and roman_string[t + 1] == 'M'):
                 t += 1
                 total += 900
-            elif (t + 1 < l and roman_string[t + 1] == 'C'):
+            elif (t + 1 < lt and roman_string[t + 1] == 'C'):
                 count = 100
-                while (t < l and t + 1 < l and roman_string[t + 1] == 'C'):
+                while (t < lt and t + 1 < lt and roman_string[t + 1] == 'C'):
                     count = count + 100
                     t += 1
                 total += count
@@ -61,9 +60,9 @@ def roman_to_int(roman_string):
         elif (idx == 'D'):
             total += 500
         elif (idx == 'M'):
-            if (t + 1 < l and roman_string[t + 1] == 'M'):
+            if (t + 1 < lt and roman_string[t + 1] == 'M'):
                 count = 1000
-                while (t < l and t + 1 < l and roman_string[t + 1] == 'M'):
+                while (t < lt and t + 1 < lt and roman_string[t + 1] == 'M'):
                     count = count + 1000
                     t += 1
                 total += count
