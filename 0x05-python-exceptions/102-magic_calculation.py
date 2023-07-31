f47ab1c36c32acd2715f5b361a8d839cbfeee435
raytchellee <amakaoforkaja@gmail.com>
2023-07-31 21:41:58 +0000

    task 9

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    total = 0
+
+    for idx in range(1, 3):
+        try:
+            if (idx > a):
+                raise Exception("Too far")
+            total += (a ** b) / idx
+        except:
+            total = a + b
+            break
+        return total
