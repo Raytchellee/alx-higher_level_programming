f0b0f720d08b9f712b86d0c534662807f0092667
raytchellee <amakaoforkaja@gmail.com>
2023-08-01 22:38:24 +0000

    task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Documentation for python class"""
+
+
+class Square:
+    """A class example for square"""
+
+    def __init__(self, size=0):
+        """initializes size as private"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
