65ef93204bb7b295a3225039fe841f37d657be3b
raytchellee <amakaoforkaja@gmail.com>
2023-08-01 22:32:16 +0000

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Documentation for python class"""
+
+
+class Square:
+    """A class example for square"""
+
+    def __init__(self, size):
+        """initializes size as private"""
+        self.__size = size
