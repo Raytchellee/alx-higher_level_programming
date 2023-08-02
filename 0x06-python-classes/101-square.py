faf92e550f4b2e993a3c06d7d2fa268f9a1839ed
raytchellee <amakaoforkaja@gmail.com>
2023-08-02 00:38:33 +0000

    task 8

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -51,7 +51,7 @@ class Square:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
-    def __str__(self):
+    def __result__(self):
         '''square of hash'''
         res = []
         if self.__size == 0:
