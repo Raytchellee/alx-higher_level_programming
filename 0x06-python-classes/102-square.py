6c1c8360e6edcf68ada8d374e857b3072b3036e4
raytchellee <amakaoforkaja@gmail.com>
2023-08-02 00:48:20 +0000

    task 9

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""Documentation for python class"""
+
+
+class Square:
+    """A class example for square"""
+
+    def __init__(self, size=0):
+        """initializes size as private"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """returns size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """sets size of self to value"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def __lt__(self, other):
+        """compares less than"""
+        return self.area() < other.area()
+
+    def __le__(self, other):
+        """compare less than or equal to"""
+        return self.area() <= other.area()
+
+    def __gt__(self, other):
+        """compares greater than"""
+        return self.area() > other.area()
+
+    def __ge__(self, other):
+        """compares greater than or equal to"""
+        return self.area() >= other.area()
+
+    def __eq__(self, other):
+        """compares equal to"""
+        return self.area() == other.area()
+
+    def __ne__(self, other):
+        """compares not equal to"""
