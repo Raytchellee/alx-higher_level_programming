1643af64abe25b14341df4e0ea5bcb07e97a090f
raytchellee <amakaoforkaja@gmail.com>
2023-08-07 23:12:32 +0000

    task 9

---
+++ 0x08-python-more_classes/9-rectangle.py
@@ -0,0 +1,113 @@
+#!/usr/bin/python3
+"""A Rectangle Class in Python"""
+
+
+class Rectangle:
+    """A typical rectangle Class definition in Python
+
+    Attributes:
+        number_of_instances: Total available instances
+        print_symbol: Symbol to print
+    """
+
+    number_of_instances = 0
+    print_symbol = "#"
+
+    def __init__(self, width=0, height=0):
+        """Initialize new Rectangle
+
+        Args:
+            width: Rectangle width
+            height: Rectangle height
+        """
+        self.width = width
+        self.height = height
+        type(self).number_of_instances += 1
+
+    @property
+    def width(self):
+        """controls width of the rectangle"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """controls height of the rectangle"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    @classmethod
+    def square(cls, size=0):
+        """Creates a Square of the given size
+
+        Args:
+            size: dimensions of the square
+        """
+        return (cls(size, size))
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        """Checks for rectangle with greater area
+
+        Args:
+            rect_1: Rectangle 1
+            rect_2: Rectangle 2
+        """
+        if type(rect_1) is not Rectangle:
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if type(rect_2) is not Rectangle:
+            raise TypeError("rect_2 must be an instance of Rectangle")
+
+        if rect_1.area() >= rect_2.area():
+            return (rect_1)
+        else:
+            return (rect_2)
+
+    def area(self):
+        """Gets rectangle area"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Gets perimeter of rectangle"""
+        if self.__height == 0 or self.__width == 0:
+            return (0)
+        return (self.__width + self.__height) * 2
+
+    def __str__(self):
+        """Prints a rectangle of # characters
+        """
+        if self.__width == 0 or self.__height == 0:
+            return ("")
+
+        hashe = []
+        for idx in range(self.__height):
+            [hashe.append(str(self.print_symbol)) for _ in range(self.__width)]
+            if idx != self.__height - 1:
+                hashe.append("\n")
+        return ("".join(hashe))
+
+    def __repr__(self):
+        """Creates a new rectangle class"""
+        res = "Rectangle("
+        res += str(self.__width) + ", "
+        res += str(self.__height) + ")"
+        return (res)
+
+    def __del__(self):
+        """Prints message for any deleted rectangle"""
+        print("Bye rectangle...")
+        type(self).number_of_instances -= 1
