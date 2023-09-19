545dc74450e0a4881abcce83d363b0d6678837da
raytchellee <amakaoforkaja@gmail.com>
2023-09-19 15:06:13 -0700

    task 102

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,3 +1,5 @@
 --  lists average of cities
-SELECT city, AVG(value) avg_temp FROM temperatures GROUP BY city 
+SELECT city, AVG(value) avg_temp FROM temperatures 
+	WHERE month between 7 and 8
+	GROUP BY city 
 	ORDER BY avg_temp DESC LIMIT 3;
