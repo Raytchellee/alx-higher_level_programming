74a04d1cf3effa5333a047504e0d4c1865352ac0
raytchellee <amakaoforkaja@gmail.com>
2023-10-17 05:18:03 -0700

    task check

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -2,7 +2,7 @@
 exports.esrever = function (list) {
   const len = list.length;
 
-  for (let idx = 0; idx < len/2; idx++) {
+  for (let idx = 0; idx < len / 2; idx++) {
     [list[idx], list[len - idx - 1]] = [list[len - idx - 1], list[idx]];
   }
 
