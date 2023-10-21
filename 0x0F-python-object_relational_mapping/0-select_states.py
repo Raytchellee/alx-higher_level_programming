af11c99fc3caa92cbcad2cccb09ac95d4a9f5f31
raytchellee <amakaoforkaja@gmail.com>
2023-10-21 07:43:32 -0700

    task check

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Displays all states from hbtn_0e_0_usa in order"""
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    """ Do not execute when imported """
+    myDb = MySQLdb.connect(host="localhost", user=argv[1],
+                        port=3306, passwd=argv[2], db=argv[3])
+    cs = myDb.cursor()
+    cs.execute("SELECT * FROM states ORDER BY id ASC")
+    data = cs.fetchall()
+    for item in data:
+        print(item)
+    cs.close()
+    myDb.close()
