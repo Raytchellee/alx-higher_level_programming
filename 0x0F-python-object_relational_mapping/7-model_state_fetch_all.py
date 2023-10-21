5987dc80b6950f648f3628d3da3d0c74ed23d9b7
raytchellee <amakaoforkaja@gmail.com>
2023-10-21 08:48:21 -0700

    task check

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""lists states from the database using orm"""
+
+from sys import argv
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    """lists states from the database using orm"""
+    uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3])
+    engine = create_engine(uri, pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+    Sesh = sessionmaker(bind=engine)
+    sesh = Sesh()
+    for item in sesh.query(State).order_by(State.id).all():
+        print("{}: {}".format(item.id, item.name))
+    sesh.close()
