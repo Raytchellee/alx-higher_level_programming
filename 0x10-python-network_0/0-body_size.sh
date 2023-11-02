cb462b273167470c08c0b14c59e5de4bb3f953db
raytchellee <amakaoforkaja@gmail.com>
2023-11-02 16:10:22 -0700

    check

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #sends a request to an input uri and displays size of response
-curl -sI '$1' | grep -i 'Content-Length' | awk '{print $2}'
+curl -sI "$1" | grep -i "Content-Length" | awk '{print $2}'
