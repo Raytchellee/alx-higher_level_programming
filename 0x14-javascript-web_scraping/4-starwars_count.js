3808d167ff378bc39145771f1123965e0b19c4da
raytchellee <amakaoforkaja@gmail.com>
2023-11-28 12:36:31 -0800

    task check

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,9 +4,9 @@ const link = process.argv[2];
 
 req(link, function (err, _, data) {
   if (!err) {
-    const res = JSON.parse(data).res;
+    const res = JSON.parse(data).results;
     console.log(res.reduce((num, film) => {
-      return film.characters.find((d) => d.endsWith('/18/'))
+      return film.characters.find((d) => d.includes('18'))
         ? num + 1
         : num;
     }, 0));
