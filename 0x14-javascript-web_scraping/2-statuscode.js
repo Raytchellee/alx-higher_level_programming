7e421ae54da59cfdec3d895e10e61ef52b71005c
raytchellee <amakaoforkaja@gmail.com>
2023-11-28 12:23:53 -0800

    task check

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const req = require('request');
-const link = process.argv[2]
+const link = process.argv[2];
 
 req.get(link).on('response', function (res) {
   console.log(`code: ${res.statusCode}`);
--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,8 +2,8 @@
 
 const req = require('request');
 const id = process.argv[2];
-let url = `https://swapi-api.alx-tools.com/api/films/${id}`;
+const link = `https://swapi-api.alx-tools.com/api/films/${id}`;
 
-req(url, function (err, res, data) {
+req(link, function (err, res, data) {
   console.log(err || JSON.parse(data).title);
 });
